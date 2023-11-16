ceaf3a8e3d10910a475230ac91b2a0b47f3f2b8a
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2023-11-16 10:05:13 -0300

    Add files via upload

---
+++ app/Http/Controllers/Api/SeriesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
+    public function index()
+    {
+        return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        return response()
+            ->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(Series $series)
+    {
+        return $series;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(int $series)
+    {
+        Series::destroy($series);
+        return response()->noContent();
+    }
+}
