21fc1641f836e5c75501d6a1feb22f13e14e32cd
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-09-01 03:24:38 -0300

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,27 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
-class LoginController
+class LoginController extends Controller
 {
+    /**
+     * Exibe a página de login.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         return view('login.index');
     }
 
+    /**
+     * Processa o login do usuário.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
-        if (!Auth::attempt($request->only(['email', 'password']))) {
-            return redirect()->back()->withErrors('Usuário ou senha inválidos');
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator)->withInput();
         }
-
-        return to_route('series.index');
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return Redirect::back()->withErrors([
+                'login_error' => 'Usuário ou senha inválidos',
+            ])->withInput();
+        }
+        return redirect()->route('series.index');
     }
 
+    /**
+     * Processa o logout do usuário.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function destroy()
     {
         Auth::logout();
-
-        return to_route('login');
+        return redirect()->route('login');
     }
 }
